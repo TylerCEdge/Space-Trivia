bc772cac8a6945630bf6cb16010174eb720840b9
tyler <tylercedge@gmail.com>
2019-02-19 15:00:31 -0500

    game styling minus scoreboard need js wired up

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// <!-- * You'll create a trivia form with multiple choice or true/false options (your choice).
+
+//     * The player will have a limited amount of time to finish the quiz.
+
+//     * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly
+//     and incorrectly.
+
+//     * Don't let the player pick more than one answer per question.
+
+//     * Don't forget to include a countdown timer. -->
+
+$(document).ready(function () {
+
+    var score = 0;
+
+    var questions = [
+        "Which one is not a type of galaxy?",
+        "What process fuels the sun?",
+        "What galaxy is made of old stars?"];
+
+    var answers0 = ["Spiral", "Irregular", "Orbital", "Elliptical"];
+    var answers1 = ["Precipitation", "Nuculear Fusion", "Condensation", "Photosynthesis"];
+    var answers2 = ["Spiral", "Irregular", "Lenticular", "Elliptical"];
+
+    
+});
