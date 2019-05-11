4d3f644671cd5b43f9c6dcc6893db11fca79561d
TylerCEdge <46033419+TylerCEdge@users.noreply.github.com>
2019-05-11 10:04:32 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,7 +62,7 @@ var game = {
     },
     start: function () {
         timer = setInterval(game.countdown, 1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">60</span> Seconds</h2>');
         $('#start').remove();
         for (var i = 0; i < questions.length; i++) {
             $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
